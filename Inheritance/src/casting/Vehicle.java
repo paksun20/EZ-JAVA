03a5a9dfc75640067da23c8f2e67e5f3e5910b5b
paksun20 <paksun20@naver.com>
2021-04-27 16:48:59 +0900

    2021-0427 마지막시간

---
+++ Inheritance/src/casting/Vehicle.java
@@ -0,0 +1,7 @@
+package casting;
+
+public class Vehicle {
+	public void run() {
+		System.out.println("자동차는 달린다.");
+	}
+}
---
+++ Inheritance/src/drevers2_instanceof/Vehicle.java
@@ -0,0 +1,7 @@
+package drivers2;
+
+public class Vehicle {
+	public void run() {
+		System.out.println("자동차는 달린다.");
+	}
+}
